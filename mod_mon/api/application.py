4b907764faf04158dd817a0fdb41dab23abf77aa
Artur Stępniak <aart.st@gmail.com>
2023-11-24 22:37:06 +0100

    test: add ruff tests

--- mod_mon/api/application.py
+++ mod_mon/api/application.py
@@ -1,3 +1,7 @@
 from fastapi import FastAPI
 
+from mod_mon.auth.api import authentication
+
 app = FastAPI(title="mod_mon API", version="0.1.0")
+
+app.include_router(authentication.router, prefix="/auth")
