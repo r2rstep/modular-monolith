f7a1dce8f9f61d834ff32bff612a676b0fc8a44f
Artur Stępniak <aart.st@gmail.com>
2024-01-08 11:31:07 +0100

    refactor: change the way public events are stored in outbox
    1. Generic event handler added that stores the events in outbox
    2. Added mediator before calling a specific handler. The mediator injects
    dependencies

--- building_blocks/within_bounded_context/domain/events.py
+++ building_blocks/within_bounded_context/domain/events.py
@@ -3,6 +3,7 @@ from datetime import datetime
 from typing import TypeVar
 
 from pydantic import BaseModel, ConfigDict, Field
+from pydantic.fields import FieldInfo
 
 
 # pydantic features are not really needed but for sake of better DevEx it's better to have all the events technicalities
@@ -19,3 +20,7 @@ class DomainEvent(ABC, BaseModel):
 
 
 DomainEventType = TypeVar("DomainEventType", bound=DomainEvent)
+
+
+def is_public_event(event_cls: type[DomainEvent]) -> bool:
+    return event_cls.model_fields.get("is_public", FieldInfo(default=True)).default
