5d97dad781c8977a86606e50a465593f13f4e6f7
Artur Stępniak <aart.st@gmail.com>
2024-01-11 16:50:47 +0100

    refactor: remove event bus from base command handler

--- building_blocks/within_bounded_context/application/command.py
+++ building_blocks/within_bounded_context/application/command.py
@@ -1,10 +1,8 @@
 from abc import ABC, abstractmethod
 from typing import Generic, TypeVar
 
-import injector
 from pydantic import BaseModel, ConfigDict
 
-from commons.event_bus.application.event_bus import EventBus
 from commons.types import PK, NoneOr
 
 
@@ -16,10 +14,6 @@ CommandType = TypeVar("CommandType", bound=Command)
 
 
 class CommandHandler(ABC, Generic[CommandType]):
-    @injector.inject
-    def __init__(self, event_bus: EventBus):
-        self.event_bus = event_bus
-
     @abstractmethod
     async def handle(self, command: CommandType) -> NoneOr[PK]:
         ...
--- modules/rich_domain/module_1/core/application/commands/rich_domain_model.py
+++ modules/rich_domain/module_1/core/application/commands/rich_domain_model.py
@@ -1,8 +1,11 @@
 from uuid import uuid4
 
+import injector
+
 from modules.rich_domain.module_1.core.domain.events import RichDomainModelCreated
 
 from building_blocks.within_bounded_context.application.command import Command, CommandHandler
+from commons.event_bus.application.event_bus import EventBus
 from commons.types import PK
 from modules.rich_domain.language import RichDomainModelName
 
@@ -12,6 +15,10 @@ class CreateRichDomainModel(Command):
 
 
 class CreateRichDomainModelHandler(CommandHandler[CreateRichDomainModel]):
+    @injector.inject
+    def __init__(self, event_bus: EventBus):
+        self.event_bus = event_bus
+
     async def handle(self, command: CreateRichDomainModel) -> PK:
         pk = PK(uuid4())
 
